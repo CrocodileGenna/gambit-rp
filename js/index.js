f71bdc5af3d22cbbfc4acbe744e7838e1ee4a4fd
Yurii <99128333+CrocodileGenna@users.noreply.github.com>
2023-02-16 15:48:56 +0200

    + modal create acc

--- js/index.js
+++ js/index.js
@@ -1,13 +1,31 @@
 const refs = {
-  openBtn: document.querySelector(".header__burger"),
-  menu: document.querySelector(".header__menu"),
   body: document.querySelector("body"),
+  openBurgerBtn: document.querySelector(".header__burger"),
+  menuBurger: document.querySelector(".header__menu"),
+
+  btnCreateAccount: document.querySelector(".content__buttons__create"),
+  modalCreate: document.querySelector("#register-modal"),
+  closeModalCreate: document.querySelector(".close"),
+  lableModalCreate: document.querySelector(".modal-content__form label"),
+  inputModalCreate: document.querySelector(".modal-content__form input"),
 };
 
-refs.openBtn.addEventListener("click", addClass);
+refs.openBurgerBtn.addEventListener("click", addClass);
+refs.btnCreateAccount.addEventListener("click", openModal);
+refs.closeModalCreate.addEventListener("click", closeModalCreate);
 
 function addClass() {
-  refs.openBtn.classList.toggle("active");
-  refs.menu.classList.toggle("active");
   refs.body.classList.toggle("lock");
+
+  refs.openBurgerBtn.classList.toggle("active");
+  refs.menuBurger.classList.toggle("active");
+}
+
+function openModal() {
+  refs.body.classList.add("lock");
+  refs.modalCreate.style.display = "block";
+}
+function closeModalCreate() {
+  refs.body.classList.remove("lock");
+  refs.modalCreate.style.display = "none";
 }
